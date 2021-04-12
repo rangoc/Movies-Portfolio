b2880a1e4720b430d37c4a4018e29b9efdb6b934
goranc <cabarkapa.goran@gmail.com>
2021-04-12 12:36:12 +0200

    Flexing on animations on About section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,31 +5,33 @@ import styled from 'styled-components';
 import { BasicLayout, Description, Image, Hide } from '../styles';
 // Framer Motion
 import { motion } from 'framer-motion';
+import { titleAnim, fade, photoAnim } from '../animation';
+
 const AboutSection = () => {
   return (
     <BasicLayout>
       <Description>
         <motion.div className="title">
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="guy with a camera" />
+        <motion.img variants={photoAnim} src={home1} alt="guy with a camera" />
       </Image>
     </BasicLayout>
   );
