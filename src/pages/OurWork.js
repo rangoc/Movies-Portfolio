89e6f5f35b086ac8216aaa28b24cae9b33621a25
goranc <cabarkapa.goran@gmail.com>
2021-04-12 10:36:03 +0200

    Styling Our Work page

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -1,7 +1,57 @@
 import React from 'react';
-
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+// Images
+import athlete from 'assets/athlete-small.png';
+import theracer from 'assets/theracer-small.png';
+import goodtimes from 'assets/goodtimes-small.png';
 const OurWork = () => {
-  return <div>Hello from OurWork</div>;
+  return (
+    <Work>
+      <Movie>
+        <h2>The Athlete</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={athlete} alt="" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={theracer} alt="" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>Good Times</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={goodtimes} alt="" />
+        </Link>
+      </Movie>
+    </Work>
+  );
 };
 
+const Work = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  h2 {
+    padding: 1rem 0rem;
+  }
+`;
+const Movie = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background: #cccccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
 export default OurWork;
