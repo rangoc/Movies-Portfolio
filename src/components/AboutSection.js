43139d9415e761bfdec7843407e4751a03d4106c
goranc <cabarkapa.goran@gmail.com>
2021-04-12 11:59:01 +0200

    Experimenting with Framer Motion on About section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,28 +3,50 @@ import home1 from 'assets/home1.png';
 // Styled
 import styled from 'styled-components';
 import { BasicLayout, Description, Image, Hide } from '../styles';
+// Framer Motion
+import { motion } from 'framer-motion';
 const AboutSection = () => {
+  const titleAnim = {
+    hidden: {
+      opacity: 0,
+    },
+    show: { opacity: 1, transition: { duration: 1 } },
+  };
+  const container = {
+    hidden: {
+      x: 100,
+    },
+    show: {
+      x: 0,
+      transition: { duration: 1, staggerChildren: 0.5, when: 'afterChildren' },
+    },
+  };
   return (
     <BasicLayout>
       <Description>
-        <div className="title">
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="title"
+        >
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true.</h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
-          <p>
-            Contact us for any photography or videography ideas that you have.
-            We have professionals with amazing skills.
-          </p>
-          <button>Contact Us</button>
-        </div>
+        </motion.div>
+        <p>
+          Contact us for any photography or videography ideas that you have. We
+          have professionals with amazing skills.
+        </p>
+        <button>Contact Us</button>
       </Description>
       <Image>
         <img src={home1} alt="guy with a camera" />
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,6 +3,7 @@ import React from 'react';
 import AboutSection from 'components/AboutSection';
 import ServicesSection from 'components/ServicesSection';
 import FaqSection from 'components/FaqSection';
+
 const AboutUs = () => {
   return (
     <>
